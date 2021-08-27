7ee7751e2a0efb3f450cc4035fc178239b24a685
Louis <louisedwardhess@icloud.com>
2021-08-27 14:56:00 -0700

    added prelim deposit method and constructor
    yes

--- ATM.java
+++ ATM.java
@@ -2,4 +2,15 @@
 public class ATM {
 	private String accountID;
 	private float balance;
+	
+
+	public ATM(String accountID, float balance) {
+		super();
+		this.accountID = accountID;
+		this.balance = balance;
+	}
+	
+	public void deposit (int deposit) {
+		balance += deposit;
+	}
 }
