adae8a7210039c7b7fd5dd047f41895f081cc882
Louis <louisedwardhess@icloud.com>
2021-08-28 10:57:05 -0700

    finished deposit method

--- ATM.java
+++ ATM.java
@@ -3,13 +3,20 @@ import java.util.HashMap;
 public class ATM {
 	private HashMap<String, Float> accounts;
 	
-
+	//TODO compatibility w/ newID creation?
 	public ATM(HashMap<String, Float> accounts) {
 		super();
 		this.accounts = accounts;
 	}
 	
 	public void deposit (String accountID, float deposit) {
-		balance += deposit;
+		if (accounts.containsKey(accountID)) {
+			accounts.replace(accountID, accounts.get(accountID) + deposit);
+		}
+		else {
+			//might break with the constructor but honestly this method makes too much sense to not use
+			String newID = "" + accounts.size();
+			accounts.put(newID, deposit);
+		}
 	}
 }
