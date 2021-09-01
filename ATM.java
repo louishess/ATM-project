0980e455618e4263f83b9e57cafad952e9a6df95
mateensharifi <mateensharifi11@gmail.com>
2021-09-01 14:39:22 -0700

    Fixed checkBalance()

--- ATM.java
+++ ATM.java
@@ -1,20 +1,21 @@
 import java.util.HashMap;
+
 public class ATM {
 	private HashMap<String, Double> accounts;
-	
+
 	public ATM() {
 		super();
 		accounts = new HashMap<String, Double>();
 	}
-	
-	public void deposit (String accountID, double deposit) throws Exception {
+
+	public void deposit(String accountID, double deposit) throws Exception {
 		if (deposit < 0) {
-			throw new IllegalArgumentException("oh. bet you think you're real clever, don't you. 'oh, it doesn't have a withdraw function, ill just deposit a negative number and it'll spit money to me. heheheeheheh!' cute. really. cute. not today buddy. you're allowed to give me money. get outta here with these negative numbers.");
+			throw new IllegalArgumentException(
+					"oh. bet you think you're real clever, don't you. 'oh, it doesn't have a withdraw function, ill just deposit a negative number and it'll spit money to me. heheheeheheh!' cute. really. cute. not today buddy. you're allowed to give me money. get outta here with these negative numbers.");
 		}
 		if (accounts.containsKey(accountID)) {
 			accounts.replace(accountID, accounts.get(accountID) + deposit);
-		}
-		else {
+		} else {
 			accounts.put(accountID, deposit);
 		}
 	}
@@ -26,15 +27,17 @@ public class ATM {
 	public void setAccounts(HashMap<String, Double> accounts) {
 		this.accounts = accounts;
 	}
-	public void withdraw (String accountID, double withdraw) {
+
+	public void withdraw(String accountID, double withdraw) {
 		if (withdraw > accounts.get(accountID)) {
-			throw new IllegalArgumentException ("You do not have sufficient funds to make this withdrawal");
+			throw new IllegalArgumentException("You do not have sufficient funds to make this withdrawal");
 		}
-			accounts.replace(accountID, accounts.get(accountID), accounts.get(accountID) - withdraw);
+		accounts.replace(accountID, accounts.get(accountID), accounts.get(accountID) - withdraw);
 	}
-	public Double checkBalance (String accountID) {
-		if(!accountID.equals(accounts.keySet())) {
-			throw new IllegalArgumentException ("Account does not exist");
+
+	public Double checkBalance(String accountID) {
+		if (accounts.containsKey(accountID) == false) {
+			throw new IllegalArgumentException("Account does not exist");
 		}
 		return accounts.get(accountID);
 	}
--- ATMTester2.java
+++ ATMTester2.java
@@ -9,11 +9,11 @@ public class ATMTester2 {
 		bank.deposit("Bezos", 193495330293.43);
 		bank.deposit("Bezos", 20394353.35);
 		bank.withdraw("Bezos", 3593293.66);
-		//bank.deposit("MrTheiss",64033.55);
-		//bank.withdraw("MrTheiss", 443964.46);
+		bank.deposit("MrTheiss",64033.55);
+		bank.withdraw("MrTheiss", 443964.46);
 		System.out.println(bank.checkBalance("Bezos"));
 		System.out.println(bank.checkBalance("Winfrey"));
-		//System.out.println(bank.checkBalance("YoMama"));
-		//System.out.println(bank.checkBalance("MrTheiss"));
+		System.out.println(bank.checkBalance("YoMama"));
+		System.out.println(bank.checkBalance("MrTheiss"));
 	}
 }
